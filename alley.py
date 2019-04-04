0d485a24d842c34ae0c014fe368eed670bae9b58
Eileen Bohen <43003611+boheneileen20@users.noreply.github.com>
2019-04-04 12:55:47 -0400

    added avatars

--- alley.py
+++ alley.py
@@ -28,12 +28,41 @@ class Alley(viz.EventClass):
 					
 		self.avatar = viz.add('vcc_female.cfg')
 		mat = viz.Matrix()
-		mat.postScale(.5,.5,.5)
+		mat.postScale(.3,.3,.3)
 		mat.postAxisAngle(0,1,0,self.theta)
 		mat.postTrans(self.x,.1,self.z);
 		self.avatar.setMatrix(mat)
 		
-		self.mode = "thirdperson"
+		self.mode = "firstperson"
+		
+		
+		self.spectator1 = viz.add('vcc_male.cfg')
+		spec1x = 3.5
+		spec1z = .9
+		spec1theta = 0
+		spec1value = 0
+		
+		mat = viz.Matrix()
+		mat.postScale(.3,.3,.3)
+		mat.postAxisAngle(0,1,0,spec1theta)
+		mat.postTrans(spec1x,.1,spec1z)
+		self.spectator1.setMatrix(mat)
+		
+		self.spectator1.state(3)
+		
+		self.spectator2 = viz.add('vcc_male2.cfg')
+		spec2x = 4.5
+		spec2z = .9
+		spec2theta = 0
+		spec2value = 0
+		
+		mat = viz.Matrix()
+		mat.postScale(.3,.3,.3)
+		mat.postAxisAngle(0,1,0,spec2theta)
+		mat.postTrans(spec2x,.1,spec2z)
+		self.spectator2.setMatrix(mat)
+		
+		self.spectator2.state(4)
 					
 	# Key pressed down event code.
 	def onKeyDown(self,key):
